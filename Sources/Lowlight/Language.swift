51f98f2d252d1346d76a032f02a4a9b2add63084
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-02-07 15:08:03 -0500

    Default arguments to fix test and also cause it's nice

--- Sources/Lowlight/Language.swift
+++ Sources/Lowlight/Language.swift
@@ -5,7 +5,7 @@ public struct Language: Codable, Hashable, Sendable {
 	public let keywords: Set<String>
 	public let symbols: Set<String>
 
-	public init(keywords: Set<String>, symbols: Set<String>) {
+	public init(keywords: Set<String> = [], symbols: Set<String> = []) {
 		self.keywords = keywords
 		self.symbols = symbols
 	}
