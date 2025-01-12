8c609b7c50a69fcd4a3b851f790ab87c132a68c0
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-01-12 07:10:01 -0500

    Let's use Swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -27,13 +27,3 @@ let package = Package(
 		),
 	]
 )
-
-let swiftSettings: [SwiftSetting] = [
-	.enableExperimentalFeature("StrictConcurrency")
-]
-
-for target in package.targets {
-	var settings = target.swiftSettings ?? []
-	settings.append(contentsOf: swiftSettings)
-	target.swiftSettings = settings
-}
