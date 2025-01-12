2eab9f233f0a743368a11cd0ae482c289c78b207
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-01-12 07:10:18 -0500

    Add a stock definition for swift

---
+++ Sources/Lowlight/Language+Definitions.swift
@@ -0,0 +1,30 @@
+extension Language {
+	public static let swift = Language(
+		keywords: [
+			"actor",
+			"async",
+			"case",
+			"class",
+			"default",
+			"enum",
+			"extension",
+			"final",
+			"for",
+			"func",
+			"guard",
+			"import",
+			"init",
+			"let",
+			"private",
+			"public",
+			"return",
+			"self",
+			"static",
+			"struct",
+			"switch",
+			"var",
+			"while",
+		],
+		lineComment: "//"
+	)
+}
